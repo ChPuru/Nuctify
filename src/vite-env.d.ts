c06e9c21e5abcda9c35ed36b19632942f2b03773
ChPuru <puruchoudhary9b@gmail.com>
2026-04-18 13:04:01 +0530

    heh

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,24 @@
+/// <reference types="vite/client" />
 
+interface Window {
+  NativeBridge: {
+    updateMetadata: (title: string, artist: string) => void;
+    updatePlaybackState: (isPlaying: boolean) => void;
+  };
+  Capacitor: any;
+  __TAURI__: any;
+  __TAURI_INTERNALS__: any;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+  readonly VITE_SPOTIFY_CLIENT_ID: string;
+  readonly VITE_DISCORD_CLIENT_ID: string;
+  readonly VITE_LASTFM_API_KEY: string;
+  readonly VITE_LASTFM_API_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
